7f84aff53fbaaff535b12f2dba09953789a1be61
Alex Fanat <alex.fanat@smus.ca>
2018-01-24 13:24:25 -0800

    Added functionality to MANUAL_CONTROL and added UNIT_DRIVETRAIN, then fixed some bugs.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MANUAL_CONTROL.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MANUAL_CONTROL.java
@@ -11,12 +11,60 @@ import com.qualcomm.robotcore.hardware.Servo;
  * Created by Adrian on 2017-11-19.
  */
 
-@Disabled
-@TeleOp(name = "Control")
+@TeleOp(name = "[ACTIVE] Control")
 public class MANUAL_CONTROL extends LinearOpMode
 {
+    DcMotor leftDriveMotor, rightDriveMotor, leftArmHingeMotor, rightArmHingeMotor;
+    Servo leftGrabberHingeServo, rightGrabberHingeServo, leftGrabberClampServo, rightGrabberClampServo;
+
     @Override public void runOpMode() throws InterruptedException
     {
+        leftDriveMotor = hardwareMap.dcMotor.get("leftDriveMotor");
+        rightDriveMotor = hardwareMap.dcMotor.get("rightDriveMotor");
 
+        rightDriveMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        leftArmHingeMotor = hardwareMap.dcMotor.get("leftArmHingeMotor");
+        rightArmHingeMotor = hardwareMap.dcMotor.get("rightArmHingeMotor");
+
+        rightArmHingeMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        leftGrabberHingeServo = hardwareMap.servo.get("leftGrabberHingeServo");
+        rightGrabberHingeServo = hardwareMap.servo.get("rightGrabberHingeServo");
+        leftGrabberClampServo = hardwareMap.servo.get("leftGrabberClampServo");
+        rightGrabberClampServo = hardwareMap.servo.get("rightGrabberClampServo");
+
+        rightGrabberClampServo.setDirection(Servo.Direction.REVERSE);
+        rightGrabberHingeServo.setDirection(Servo.Direction.REVERSE);
+
+        float powerMultiplier = .3f;
+
+        waitForStart();
+        new Thread(() -> {
+            while (opModeIsActive())
+            {
+                leftGrabberHingeServo.setPosition(leftGrabberHingeServo.getPosition() - gamepad2.left_stick_y * 0.01f);
+                rightGrabberHingeServo.setPosition(leftGrabberHingeServo.getPosition());
+
+                leftGrabberClampServo.setPosition(leftGrabberClampServo.getPosition() + gamepad2.right_stick_y * 0.01f);
+                rightGrabberClampServo.setPosition(leftGrabberClampServo.getPosition());
+
+                leftArmHingeMotor.setPower((gamepad2.right_trigger - gamepad2.left_trigger) * 0.1f);
+                rightArmHingeMotor.setPower(leftArmHingeMotor.getPower());
+            }
+        }).start();
+        while (opModeIsActive())
+        {
+            leftDriveMotor.setPower(powerMultiplier * (-gamepad1.left_stick_y + gamepad1.left_stick_x));
+            rightDriveMotor.setPower(powerMultiplier * (-gamepad1.left_stick_y - gamepad1.left_stick_x));
+
+            if (gamepad1.dpad_up && Math.abs(powerMultiplier) != 1f)
+                powerMultiplier += .05;
+            else if (gamepad1.dpad_down && Math.abs(powerMultiplier) != 0.5f)
+                powerMultiplier -= .05;
+
+            if (gamepad1.a)
+                powerMultiplier *= -1f;
+        }
     }
 }
